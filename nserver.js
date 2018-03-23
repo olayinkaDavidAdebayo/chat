53692bf983f768482e2c167fca2799e3c3530d37
Adebayo Olayinka David <david4real_chris@yahoo.com>
2018-03-23 18:19:44 +0000

    nserver

--- nserver.js
+++ nserver.js
@@ -5,39 +5,6 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var sid = new Array();
 
-app.get('./app/routes', function(req, res){
-  res.sendFile('index.html');
-});
-
-io.on('connection', function(socket){
-	
-	socket.on('message', function(msg){
-	   
-		//send to all on the server
-	    // io.emit('message', msg);
-
-	   	//send to only sender
-	    //io.to(socket.id).emit('message', msg+" yourself"); or socket.emit("message", msg+" yourself");
-
-	    //store socket id for later use
-	    //sid.push(socket.id);
-
-	    //send to all in the group except sender
-	    socket.broadcast.emit('message', msg);
-	});
-
-  	socket.on("disconnect", function()
-	{
-		console.log("Socket: "+socket.id+" just disconnected!");
-	});
-});
-
-io.on("connect", function(socket)
-{
-	console.log("Socket:"+socket.id+" just joined!");
-});
-
-
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
