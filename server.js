21b5f9b221fc75250a2c6f58bd903d753ff4b461
Adebayo Olayinka David <david4real_chris@yahoo.com>
2018-03-23 20:56:28 +0000

    check

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+io.on('connect', function (socket) {
+    console.log("Server started on port 3000");
+});
