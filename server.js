fdd258fafff2fb9b3037f0c2adc52b9c77dc5d8d
Adebayo Olayinka David <david4real_chris@yahoo.com>
2018-03-23 21:00:29 +0000

    y

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-var app = require('express')();
-var server = app.listen(3000);
+var io = require('socket.io')(80);
 
-var io = require('socket.io').listen(server);
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-io.on('connect', function (socket) {
-    console.log("Server started on port 3000");
+io.on('connection', function (socket) {
+  console.log("test server running")
 });
