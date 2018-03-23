4ab5b9ed7204d9b9f22a59b3a6562f3cf7491f06
Adebayo Olayinka David <david4real_chris@yahoo.com>
2018-03-23 20:55:10 +0000

    t

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ var app = require('express')();
 var server = app.listen(3000);
 
 var io = require('socket.io').listen(server);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
