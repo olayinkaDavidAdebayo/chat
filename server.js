f108a662e4c96f74e96bf7a66f61dd81da023bc0
Adebayo Olayinka David <david4real_chris@yahoo.com>
2018-03-23 20:34:08 +0000

    socket.io

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-var app = require('express')();
-var server = app.listen(3000);
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-var io = require('socket.io').listen(server);
+app.listen(80);
 
-io.on('connection', function(socket)
-{
-	socket.on('message', function(msg){
-     console.log("Socket:"+socket.id+" just joined!");
- 	});
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
 }
-);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
